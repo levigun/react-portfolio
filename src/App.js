011620bba557b611fbad976a451418f1735716e7
levigun <t.levina31@gmail.com>
2022-05-01 14:28:11 +1000

    deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 // import Home from './components/Home'
 // import About from './components/About'
 // import Contact from './components/Contact'
-import Layout from './components/Layout'
+import PortfolioContainer from './components/PortfolioContainer'
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<PortfolioContainer />}>
         {/* <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="/contact" element={<Contact />} /> */}
--- src/components/PortfolioContainer/index.js
+++ src/components/PortfolioContainer/index.js
@@ -1,5 +1,4 @@
-import { Outlet } from 'react-router-dom'
-import Navbar from '../Navbar/'
+import Navbar from '../Navbar'
 import './index.scss'
 
 const Layout = () => {
